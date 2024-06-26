fb240872b348af84c0018e715601a8da400f3260
Luis02022 <160199124+Luis02022@users.noreply.github.com>
2024-06-26 13:50:18 +0000

    Exemplo

---
+++ Atividade/exemplo 4.0.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+import os 
+
+os.system("cls || clear")
+
+QUANT = 2 
+
+
+@dataclass
+class Aluno:
+    nome: str 
+    idade: int
+
+alunos = []
+
+for i in range (QUANT):
+    aluno = Aluno(
+        nome = input("Digite seu nome:"),
+        idade = input("Digite sua idade:")
+    
+    )
+    alunos.append(aluno)
+
+arquivo = 'alunos.txt'
+
+with open(arquivo,'w') as arquivo:
+    for aluno in alunos:
+        arquivo.write(f"Nome: {aluno.nome}\nIdade: {aluno.idade}\n")
+
+print("Dados Gravados com sucesso")        
